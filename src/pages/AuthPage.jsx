9775aacadb4af40ba36ff786a0085a146d3291ed
Hitesh Pathak <hitesh@vedicmindai.in>
2026-06-08 20:17:03 +0530

    add Link import to AuthPage

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
