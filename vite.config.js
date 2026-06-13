3a68202131d96c98511593dcae233f8c2ef307a9
pathak32 <pathak32032@gmail.com>
2026-06-13 06:07:18 +0530

    Fix vite.config - no base44 plugin

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,15 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
   },
 })
