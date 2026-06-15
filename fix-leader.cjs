7c231acef68f80ace321d5f6aa5ced566785e4e3
Hitesh Pathak <hitesh@vedicmindai.in>
2026-06-15 16:07:59 +0530

    chore: remove temp helper scripts

--- fix-leader.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('src/pages/LeaderboardPage.jsx','utf8');
-c = c.replace(
-  'import {\nimport { useLanguage } from \'@/lib/LanguageContext\';\n',
-  'import { useLanguage } from \'@/lib/LanguageContext\';\nimport {\n'
-);
-fs.writeFileSync('src/pages/LeaderboardPage.jsx', c);
-console.log('Fixed!');
--- fix-olympiad.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('src/pages/OlympiadPage.jsx','utf8');
-c = c.replace(
-  'import {\nimport { useLanguage } from \'@/lib/LanguageContext\';\n',
-  'import { useLanguage } from \'@/lib/LanguageContext\';\nimport {\n'
-);
-fs.writeFileSync('src/pages/OlympiadPage.jsx', c);
-console.log('Fixed!');
